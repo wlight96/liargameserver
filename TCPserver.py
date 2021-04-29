2b1355ad019acc3396c07a3cbfbc5a12124b227a
wlight96 <wlight96@naver.com>
2021-04-29 22:21:18 +0900

    test

--- TCPserver.py
+++ TCPserver.py
@@ -15,19 +15,20 @@ print ('The server is ready to receive')
 connectionSocket, addr = serverSocket.accept()
 ok = 1
 
-
 while ok != 0:
     # data소캣으로 부터 client가 보낸 msg를 recv API로 받는다.
     data = connectionSocket.recv(1024)
     decode_data = data.decode()
-    json_data = json.loads(decode_data) 
-    ok = json_data["ok"]
+    json_data = json.loads(decode_data.encode("utf-8")) 
+    
+    ok = int(json_data["ok"])
     if ok == 0:
         # data socket을 닫아준다.
         connectionSocket.close()
         break
     sentence = json_data["sentence"]
     print(sentence)
+
     # 대문자로 받은 문자열 변형
     #capitalizedSentence = data.upper() 
     capitalizedSentence = sentence.upper()
@@ -35,6 +36,7 @@ while ok != 0:
     send_data = {
         "sentence" : capitalizedSentence
     }
+    msg = json.dumps(send_data)
     # send API를 통해 clientsocket으로 전송
     #connectionSocket.send(bytes(capitalizedSentence,encoding="utf-8"))
-    connectionSocket.send(json.dumps(send_data).encode())
+    connectionSocket.send(msg.encode())
